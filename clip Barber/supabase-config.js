92f106f253e38fe04c7f5b641513f420cdd45270
Facundo Duarte <facuduartedev@gmail.com>
2026-08-21 00:09:47 -0300

    .

--- clip Barber/supabase-config.js
+++ clip Barber/supabase-config.js
@@ -1,4 +1,6 @@
 /* Pegá aquí los datos públicos de tu proyecto Supabase (Settings > API).
    Nunca uses la clave service_role en una web. */
+Object.defineProperty(window, 'CLIP_SUPABASE_URL', { get: () => NEXT_PUBLIC_SUPABASE_URL });
+Object.defineProperty(window, 'CLIP_SUPABASE_PUBLISHABLE_KEY', { get: () => NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY });
 NEXT_PUBLIC_SUPABASE_URL='https://dhhjabdctvofsymtxyal.supabase.co';
 NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY='sb_publishable_AlfwOwHLBhIMGs4tTRdIQQ_yo5UOCPf';
